636b69c4250d8a83d09eff2138104444cf0bc604
Mohammad Naeimi <mohammad.n@email.com>
2021-09-09 18:54:35 +0430

    create ps system call

--- proc.c
+++ proc.c
@@ -44,7 +44,7 @@ mycpu(void)
   
   apicid = lapicid();
   // APIC IDs are not guaranteed to be contiguous. Maybe we should have
-  // a reverse map, or reserve a register to store &cpus[i].
+    // a reverse map, or reserve a register to store &cpus[i].
   for (i = 0; i < ncpu; ++i) {
     if (cpus[i].apicid == apicid)
       return &cpus[i];
@@ -531,4 +531,18 @@ procdump(void)
     }
     cprintf("\n");
   }
+}
+
+int cps (void) {
+  struct proc *p;
+  sti();
+  acquire(&ptable.lock);
+  cprintf("name \t pid \t state \t priority \n");
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+    if (p->state == SLEEPING) cprintf("%s \t %d \t SLEEPING \t %d \n ", p->name, p->pid);
+    else if (p->state == RUNNING) cprintf("%s \t %d \t RUNNING \t %d \n ", p->name, p->pid);
+    else if (p->state == RUNNABLE) cprintf("%s \t %d \t RUNNABLE \t %d \n ", p->name, p->pid);
+  }
+  release(&ptable.lock);
+  return 22;
 }
