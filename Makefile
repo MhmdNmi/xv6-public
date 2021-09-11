44331e9997904d1d1766e08458ccb64a38681dfe
Mohammad Naeimi <mohammad.n@email.com>
2021-09-11 16:20:06 +0430

    pririty scheduler

--- Makefile
+++ Makefile
@@ -182,6 +182,8 @@ UPROGS=\
 	_wc\
 	_zombie\
 	_ps\
+	_nice\
+	_dpro\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -251,7 +253,7 @@ qemu-nox-gdb: fs.img xv6.img .gdbinit
 EXTRA=\
 	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c\
 	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c\
-	ps.c\
+	ps.c nice.c dpro.c\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
 	.gdbinit.tmpl gdbutil\
--- proc.c
+++ proc.c
@@ -88,6 +88,7 @@ allocproc(void)
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
+  p->priority = 10;  //Default Priority of a process is set to be 10
 
   release(&ptable.lock);
 
@@ -322,23 +323,29 @@ wait(void)
 void
 scheduler(void)
 {
-  struct proc *p;
+  struct proc *p, *p1;
   struct cpu *c = mycpu();
   c->proc = 0;
   
   for(;;){
     // Enable interrupts on this processor.
     sti();
-
+    struct proc *highP;
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+      if(p->state != RUNNABLE) continue;
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
+      highP = p;
+      //choose one with highest priority
+      for(p1 = ptable.proc; p1 < &ptable.proc[NPROC]; p1++){
+        if(p1->state != RUNNABLE) continue;
+        if(highP->priority > p1->priority) highP = p1;  //larger value, lower priority
+      }
+      p = highP;
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
@@ -539,10 +546,23 @@ int cps (void) {
   acquire(&ptable.lock);
   cprintf("name \t pid \t state \t priority \n");
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
-    if (p->state == SLEEPING) cprintf("%s \t %d \t SLEEPING \t %d \n ", p->name, p->pid);
-    else if (p->state == RUNNING) cprintf("%s \t %d \t RUNNING \t %d \n ", p->name, p->pid);
-    else if (p->state == RUNNABLE) cprintf("%s \t %d \t RUNNABLE \t %d \n ", p->name, p->pid);
+    if (p->state == SLEEPING) cprintf("%s \t %d \t SLEEPING \t %d \n ", p->name, p->pid, p->priority);
+    else if (p->state == RUNNING) cprintf("%s \t %d \t RUNNING \t %d \n ", p->name, p->pid, p->priority);
+    else if (p->state == RUNNABLE) cprintf("%s \t %d \t RUNNABLE \t %d \n ", p->name, p->pid, p->priority);
   }
   release(&ptable.lock);
   return 22;
+}
+
+int chpr(int pid, int priority) {
+	struct proc *p;
+	acquire(&ptable.lock);
+	for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+	  if(p->pid == pid){
+			p->priority = priority;
+			break;
+		}
+	}
+	release(&ptable.lock);
+	return pid;
 }
